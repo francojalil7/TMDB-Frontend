8c73e1cdcd565920d79d6cb55ec810ae9e79a7c1
francojalil7 <francojalil7@gmail.com>
2022-10-14 13:46:22 -0300

    add navbar responsive

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -19,8 +19,8 @@ import {
 import { HamburgerIcon, CloseIcon, AddIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
-// const Links = ["Dashboard", "Projects", "Team"];
 import navStyles from "./NavBar.module.css";
+
 const Links = [
   {
     name: "Home",
@@ -28,11 +28,11 @@ const Links = [
   },
   {
     name: "Movies",
-    path: "/todos",
+    path: "/movies",
   },
   {
     name: "Series",
-    path: "/users/",
+    path: "/series/",
   },
 ];
 
@@ -57,14 +57,25 @@ export default function Navbar() {
     <div className={navStyles.mobileNav}>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <IconButton
-            size={"md"}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={"Open Menu"}
-            display={{ md: "none" }}
-            onClick={isOpen ? onClose : onOpen}
-          />
           <HStack spacing={8} alignItems={"center"}>
+            <Box display={{ base: "flex", md: "none" }}>
+              <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label="Options"
+                icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                variant="outline"
+                onClick={isOpen ? onClose : onOpen}
+              />
+              <MenuList>
+                <MenuItem>Home</MenuItem>
+                <MenuDivider />
+                <MenuItem>Movies</MenuItem>
+                <MenuItem>Series</MenuItem>
+              </MenuList>
+            </Menu>
+            </Box>
+            
             <Heading>TMDB</Heading>
             <HStack
               as={"nav"}
@@ -90,7 +101,7 @@ export default function Navbar() {
                 <Avatar
                   size={"sm"}
                   src={
-                    "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDNJbc8se31c5elVFS8lDzvZteW3c07LClvNaVzi3OfA&s"
                   }
                 />
               </MenuButton>
@@ -104,17 +115,6 @@ export default function Navbar() {
           </Flex>
         </Flex>
 
-        {isOpen ? (
-          <Box pb={4} display={{ md: "none" }}>
-            <Stack as={"nav"} spacing={4}>
-              {Links.map(({ name, path }) => (
-                <NavLink key={path} path={path}>
-                  {name}
-                </NavLink>
-              ))}
-            </Stack>
-          </Box>
-        ) : null}
       </Box>
 
       {/* <Box p={4}>Main Content Here</Box> */}
